a869c06b08c41b38074006d48175ad9a2ff9df58
eivinfu <eivind.furuberg@gmail.com@>
2017-12-13 14:15:08 +0100

    add tip and progress counters

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
-    <h1>{{ kana.kana }}</h1>
+    <h1 id="kana">{{ kana.kana }}</h1>
+    <h2 id="tip" v-if="showTip">{{ tip }}</h2>
+    <br v-else>
     <input
-      ref="input"
       v-bind:value="value"
-      v-on:input="updateValue($event.target.value)">
-    <h2>{{ value }}</h2>
+      v-on:input="updateValue($event.target.value)"
+    ><br>
+    <span v-if="correct">Correct: {{ correct }}</span>
+    <span v-if="wrong">Wrong: {{ wrong }}</span>
+    <br v-if="!correct || !wrong">
   </div>
 </template>
 
@@ -17,20 +21,32 @@ export default {
   data () {
     return {
       kana: hiragana[0],
-      index: 1,
+      index: 0,
+      tip: hiragana[0].romaji,
+      showTip: false,
+      correct: 0,
+      wrong: 0,
     }
   },
   props: ['value'],
   methods: {
     nextKana: function() {
-      this.kana = hiragana[this.index++]
+      this.index === hiragana.length ? this.index = 0 : this.index++
+      this.kana = hiragana[this.index]
+      this.tip = this.kana.romaji
+      this.showTip ? this.wrong++ : this.correct++
+      this.showTip = false
     },
     updateValue: function(value) {
       if (value === this.kana.romaji) {
         this.nextKana()
         this.$emit('input', '')
+        return
+      } else if (value.length === this.kana.romaji.length) {
+        this.showTip = true
+        return this.$emit('input', value)
       }
-      this.$emit('input', value)
+      return this.$emit('input', value)
     }
   }
 }
@@ -43,24 +59,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 3rem;
+  padding-bottom: 2rem;
+  border: 1px solid black;
+  border-radius: 1rem;
 }
 
-h1, h2 {
+#tip {
+  line-height: 0;
   font-weight: normal;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+#kana {
+  font-size: 4rem;
+  font-weight: 500;
 }
 </style>
