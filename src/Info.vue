71542c8f54fe54bdda2f06c83e3761389d3e41e6
eivinfu <eivind.furuberg@gmail.com@>
2018-01-30 21:10:54 +0100

    Added more info

--- src/Info.vue
+++ src/Info.vue
@@ -5,6 +5,14 @@
       Then under practise type the corresponding romaji,<br>
       for the displayed kana.
     </p>
+    <p>
+      This is version 0.0.1, purely a functional demo.
+    </p>
+    <p>
+      If you don't know the answer just keep typing, <br>
+      the answer will show if your input is the same length <br>
+      but not the same letters as the correct romaji.
+    </p>
   </div>
 </template>
 
